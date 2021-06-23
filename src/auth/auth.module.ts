28a49935f5e81f67e5424372d6f5075b31e74efd
Antonio Tobajas <atobajas@gmail.com>
2021-06-23 18:20:56 +0200

    Añadido  JWT

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { UsersService } from './users.service';
-import { LocalStrategy } from './local-strategy.service';
 import { AuthController } from './auth.controller';
+import { UsersService } from './users.service';
+import { LocalStrategyService } from './local-strategy.service';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from './constants';
+import { JwtStrategyService } from './jwt-strategy.service';
 
 @Module({
-  providers: [AuthService, UsersService, LocalStrategy],
+  imports: [
+    PassportModule,
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '60s' },
+    }),
+  ],
+  providers: [
+    AuthService,
+    UsersService,
+    LocalStrategyService,
+    JwtStrategyService,
+  ],
   controllers: [AuthController],
+  exports: [AuthService],
 })
 export class AuthModule {}
